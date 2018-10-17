1d1927665c7df661fc4906dc6ace74f9b79bc54f
andreisid <andrei_sid@yahoo.com>
2018-10-18 01:05:41 +0300

    constructor+factory

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,5 +1,29 @@
 //comment
 let greet = "hey";
-console.log(greet);
+//console.log(greet);
 let nb = 0.3;
-console.log(nb + 1);
+//console.log(nb + 1);
+
+//Factory function
+function createCircle(radius) {
+  c = {
+    radius,
+    draw: function() {
+      console.log("draw");
+    }
+  };
+  return c;
+}
+c1 = createCircle(9);
+console.log(c1);
+
+//Constructor
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log("draw again");
+  };
+}
+
+c2 = new Circle(3);
+console.log(c2);
