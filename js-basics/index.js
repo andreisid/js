10f24b6919bb7dba68883d8b7a698c101b5bb801
andreisid <andrei_sid@yahoo.com>
2018-10-20 22:39:08 +0300

    tested out some more basic stuff

--- js-basics/index.js
+++ js-basics/index.js
@@ -28,4 +28,39 @@ function Circle(radius) {
 c2 = new Circle(3);
 console.log(c2);
 
-let x = {};
+//clone object
+c3 = Object.assign({}, c2);
+console.log(c3);
+
+c4 = { ...c3 };
+console.log(c4);
+
+//foreach
+const video = {
+  title: "vid1",
+  tags: ["a", "b", "c"],
+  showTags() {
+    this.tags.forEach(tag => {
+      console.log(this.title, tag);
+    });
+  }
+};
+
+video.showTags();
+
+//array
+sir = [1, 2, 3, 4, 5, 6, 7];
+sir1 = sir.filter(n => {
+  if (n % 2 == 1) return n;
+});
+console.log(sir1);
+
+sir2 = sir.map(n => {
+  return "<li>" + n + "</li>";
+});
+
+console.log(sir2);
+
+sir3 = sir.map(n => "<li>" + n + "</li>");
+
+console.log(sir3);
