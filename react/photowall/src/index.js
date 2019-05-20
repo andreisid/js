72fb812ff336cb9263555b50942108ad46fe0992
andreisid <andrei_sid@yahoo.com>
2019-05-20 22:43:25 +0300

    added some basic components

--- react/photowall/src/index.js
+++ react/photowall/src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 //import App from "./App";
 
-const tasks = ["aaa", "bbb", "ccc"];
+const tasks = ["wake up", "eat", "wash", "go to work"];
 
 const element = (
   <ol>
@@ -11,6 +11,22 @@ const element = (
     ))}
   </ol>
 );
+
+class List extends Component {
+  render() {
+    <ol>
+      {tasks.map((e, i) => (
+        <li key={i}>{e}</li>
+      ))}
+    </ol>;
+  }
+}
+
+class Title extends Component {
+  render() {
+    return <h1>tasks list</h1>;
+  }
+}
 
 ReactDOM.render(element, document.getElementById("root"));
 // ReactDOM.render(
